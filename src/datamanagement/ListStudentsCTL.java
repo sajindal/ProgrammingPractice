6a9c8046e865c90f9e9d78ff98b3d84406f248a6
Vaio <Vaio@Vaio-VAIO>
2016-09-16 06:52:22 +0530

    change the private variable, add underscore at the end.

--- src/datamanagement/ListStudentsCTL.java
+++ src/datamanagement/ListStudentsCTL.java
@@ -1,8 +1,8 @@
 package datamanagement;
         public class ListStudentsCTL {
-private StudentManager sm;
-public ListStudentsCTL() {sm = StudentManager.get();}
+private StudentManager sm_;
+public ListStudentsCTL() {sm_ = StudentManager.get();}
             public void listStudents( IStudentLister lister, String unitCode ) {
     lister.clearStudents();
-                StudentMap students = sm.getStudentsByUnit( unitCode );
+                StudentMap students = sm_.getStudentsByUnit( unitCode );
 for (Integer id : students.keySet() ) lister.addStudent(students.get(id));}}
