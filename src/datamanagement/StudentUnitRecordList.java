def8cb7c3575bc1ea851d8757cb6f21992acbc53
Vaio <Vaio@Vaio-VAIO>
2016-09-16 07:30:36 +0530

    gives spaces in braces

--- src/datamanagement/StudentUnitRecordList.java
+++ src/datamanagement/StudentUnitRecordList.java
@@ -1,3 +1,6 @@
 package datamanagement;
 
-public class StudentUnitRecordList extends java.util.ArrayList<IStudentUnitRecord> {}
+public class StudentUnitRecordList extends
+java.util.ArrayList<IStudentUnitRecord> {
+	
+}
