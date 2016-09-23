60fca4fe3382464094c20815b18bb32f4eb1f84a
sahil <jay.rihan33@gmail.com>
2016-09-23 06:15:05 +0530

    make changes in braces

--- src/datamanagement/AppProperties.java
+++ src/datamanagement/AppProperties.java
@@ -5,10 +5,14 @@ public class AppProperties {
 private static AppProperties self = null;
 private Properties properties;
 
-public static AppProperties getInstance() {
-if (self == null ) { self = new AppProperties(); } return self;}
+public static AppProperties getInstance()
+{
+if (self == null ) { self = new AppProperties(); } return self;
+}
 private AppProperties() {properties = new Properties();
-try {properties.load(new FileInputStream("Properties.prop"));} 
+try {properties.load(new FileInputStream("Properties.prop"));
+} 
 catch (IOException e) {throw new RuntimeException("Could not read property file");}}
     public Properties getProperties() {return properties;
-}}
+}
+    }
