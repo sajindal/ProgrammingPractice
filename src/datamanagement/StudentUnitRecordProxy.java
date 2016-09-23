cd9836deae8c9195e9d26e06cf62237f05011946
sahil <jay.rihan33@gmail.com>
2016-09-23 12:03:16 +0530

    give spaces between method and name

--- src/datamanagement/StudentUnitRecordProxy.java
+++ src/datamanagement/StudentUnitRecordProxy.java
@@ -1,20 +1,26 @@
 package datamanagement;
-public class StudentUnitRecordProxy implements IStudentUnitRecord {
+public class StudentUnitRecordProxy implements IStudentUnitRecord
+{
 private Integer studentID;
 private String unitCode;
 private StudentUnitRecordManager mngr;
 public StudentUnitRecordProxy( Integer id, String code ) 
 {
 this.studentID = id;this.unitCode = code;
-this.mngr = StudentUnitRecordManager.instance();}
-public Integer getStudentID() { return studentID;}
+this.mngr = StudentUnitRecordManager.instance();
+}
+public Integer getStudentID() { return studentID;
+}
 public String getUnitCode() { return unitCode; 
 }
-public void setAsg1(float mark) {
-mngr.getStudentUnitRecord( studentID, unitCode ).setAsg1(mark);}
+public void setAsg1(float mark) 
+{
+mngr.getStudentUnitRecord( studentID, unitCode ).setAsg1(mark);
+}
 public float getAsg1() 
 {
-return mngr.getStudentUnitRecord( studentID, unitCode ).getAsg1();}
+return mngr.getStudentUnitRecord( studentID, unitCode ).getAsg1();
+}
 public void setAsg2(float mark) { mngr.getStudentUnitRecord( studentID, unitCode ).setAsg2(mark);}
 public float getAsg2() {return mngr.getStudentUnitRecord( studentID, unitCode ).getAsg2();
 }
@@ -22,5 +28,10 @@ public void setExam(float mark) {mngr.getStudentUnitRecord( studentID, unitCode 
 }
 public float getExam() 
 {
-return mngr.getStudentUnitRecord( studentID, unitCode ).getExam();}
-public float getTotal() {return mngr.getStudentUnitRecord( studentID, unitCode ).getTotal();}}
+return mngr.getStudentUnitRecord( studentID, unitCode ).getExam();
+}
+public float getTotal() {return mngr.getStudentUnitRecord( studentID, unitCode ).getTotal();
+
+
+}
+}
